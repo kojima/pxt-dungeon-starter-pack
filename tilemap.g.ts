d3fd67f622228fef0014b241b30948b186f32e08
kojima <hide.k0712@gmail.com>
2023-06-26 01:42:21 -0700

    MakeCodeプロジェクトの初期ファイル

---
+++ tilemap.g.ts
@@ -0,0 +1,52 @@
+// 自動生成されたコードです。変更しないでください。
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "レベル1":
+            case "レベル1":return tiles.createTilemap(hex`0a000a0001020302020202020202020204020202020202020303030202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202`, img`
+. . 2 . . . . . . . 
+. . . . . . . . . . 
+2 2 2 . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.stairSouth,sprites.castle.tilePath5,sprites.dungeon.floorDark0,myTiles.tile2], TileScale.Sixteen);
+            case "レベル2":
+            case "レベル2":return tiles.createTilemap(hex`0800080000000000000000000000000100000000000000000000000000000000000100000001000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+`, [myTiles.transparency16,sprites.builtin.forestTiles0], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+        }
+        return null;
+    })
+
+}
+// 自動生成されたコードです。変更しないでください。
